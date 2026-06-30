fd8d6fc678d085035b1e075bdc4342fa8fe5c18c
Nguyen Duc Hung <nguyenduchung1732008@gmail.com>
2026-06-30 09:29:22 +0700

    Added features

--- hello.py
+++ hello.py
@@ -5,10 +5,12 @@ def anagramMappings(nums1, nums2):
         if k in countMap:
             countMap[k] += 1
         else:
-            countMap[k] = v
+            countMap[k] = 1
 
     for i in nums2:
         if i in countMap and countMap[i] > 0:
             arr.append(nums2.index(i))
-            countMap[i] -= 1
+            print(f"Mapping {i} from nums1 to index {nums2.index(i)} in nums2")
     return arr
+    #add new feature to the code
+    #give the user the option to input their own lists of numbers for nums1 and nums2
